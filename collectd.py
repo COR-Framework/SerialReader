0be25c9991bc6c7d73f2d7b4b5af1e1ca7e6bc6f
Denis Lavrov <bahus.vel@gmail.com>
2016-04-19 15:47:45 +1200

    using new cor implementation

--- collectd.py
+++ collectd.py
@@ -1,11 +1,14 @@
-__author__ = 'denislavrov'
+import datetime
 
-from cor.api import Message, CORModule
+from cor.api import CORModule
+from .sensor_pb2 import SensorReading
 import threading
 import time
 import socket
-import select
 import sys
+
+__author__ = 'denislavrov'
+
 
 def recvall(sock):
 	data = b""
@@ -28,27 +31,28 @@ class Collectd(CORModule):
 			time.sleep(self.period)
 			self.s.sendall("LISTVAL\n".encode("ASCII"))
 			datain = recvall(self.s).decode("UTF-8")
+			vals_to_send = {}
 			for dp in datain.split("\n"):
 				if ("found" not in dp) and dp is not "":
 					(tstamp, space, identifier) = dp.partition(" ")
 					self.s.sendall(("GETVAL " + identifier + "\n").encode("ASCII"))
 					values = recvall(self.s).decode("UTF-8")
 					lines = values.split("\n")
-					vals = {}
+					stype = identifier.partition("/")[2]
 					for val in lines:
 						if ("found" not in val) and val is not "":
 							(n, s, d) = val.partition("=")
-							vals[n] = d
-					stype = identifier.partition("/")[2]
-					if stype not in self.pvals or self.pvals[stype] != tstamp:
-						self.pvals[stype] = tstamp
-						payload = {"type": stype, "timestamp": tstamp}
-						payload.update(vals)
-						self.messageout(Message("SENSOR_READING", payload))
+							vals_to_send[stype+"/"+n] = float(d)
+			reading = SensorReading()
+			reading.location = self.location
+			reading.timestamp = int(time.time())
+			reading.values.update(vals_to_send)
+			self.messageout(reading)
 
-	def __init__(self, collectd_socket="/var/run/collectd-unixsock", period=1, *args, **kwargs):
+	def __init__(self, location="UNKNOWN", collectd_socket="/var/run/collectd-unixsock", period=1, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.period = period
+		self.location = location
 		self.s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 		try:
 			self.s.connect(collectd_socket)
@@ -58,5 +62,3 @@ class Collectd(CORModule):
 			self.s.connect(collectd_socket)
 		self.t = threading.Thread(target=self.check)
 		self.t.start()
-		self.pvals = {}
-		self.add_topics({"SENSOR_FORCE_CHECK": self.force_check})
